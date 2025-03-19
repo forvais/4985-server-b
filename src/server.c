707bc9d55488388fdc274031be22c696f9352a9e
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-03-19 06:03:24 -0700

    refactor: cleanup server driver file

--- src/server.c
+++ src/server.c
@@ -1,35 +1,30 @@
 #include "args.h"
-#include "fsm.h"
 #include "messaging.h"
 #include "networking.h"
 #include "utils.h"
-#include <errno.h>
 #include <memory.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 
 #define BACKLOG 5
 
 int main(int argc, char *argv[])
 {
-    int    retval;
     int    server_fd;
     int    sm_fd;
     args_t args;
     int    err;
 
-    setup_signal();
-
-    printf("Server launching... (press Ctrl+C to interrupt)\n");
-
-    retval = EXIT_SUCCESS;
-    err    = 0;
+    err = 0;
 
     memset(&args, 0, sizeof(args_t));
     get_arguments(&args, argc, argv);
     validate_arguments(argv[0], &args);
+
+    printf("Server launching... (press Ctrl+C to interrupt)\n");
+
+    // Setup signals
+    setup_signal();
 
     // Start TCP Server
     server_fd = tcp_server(args.addr, args.port, BACKLOG, &err);
@@ -41,22 +36,21 @@ int main(int argc, char *argv[])
 
     printf("Listening on %s:%d\n", args.addr, args.port);
 
-    // Start TCP Client
+    // Start TCP Client -- Connect to the server manager
     sm_fd = tcp_client(args.sm_addr, args.sm_port, &err);
-    // if(sm_fd < 0)
-    // {
-    //     fprintf(stderr, "main::tcp_server: Failed to create TCP server.\n");
-    //     return EXIT_FAILURE;
-    // }
+    if(sm_fd < 0)
+    {
+        fprintf(stderr, "main::tcp_client: Failed to create client socket to the Server Manager.\n");
+        return EXIT_FAILURE;
+    }
 
-    printf("Connect to server manager at %s:%d\n", args.sm_addr, args.sm_port);
+    printf("Connected to server manager at %s:%d\n", args.sm_addr, args.sm_port);
 
     // Wait for client connections
-    errno = 0;
-    err   = 0;
+    err = 0;
     event_loop(server_fd, sm_fd, &err);
 
     close(sm_fd);
     close(server_fd);
-    return retval;
+    return EXIT_FAILURE;
 }
